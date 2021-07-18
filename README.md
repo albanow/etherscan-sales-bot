f813cadf3b7bb786b67d4e32a562236d41d539e3
Albanow <albanow10@gmail.com>
2021-07-17 21:44:40 -0500

    Code formmating and readme update

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# etherscan_sales_bot
-Sales bot for Twitter directly from etherscan transfers
+# Etherscan sales bot for Python!
+
+A bot :robot: to post NFT/Token sales from etherscan transactions to Twitter.
+
+## Installation
+Clone the repo
+```
+git clone https://github.com/albanow/etherscan_sales_bot.git
+```
+Move to a virtual environment
+```
+pipenv shell
+```
+
+Install dependencies
+```
+pip install -r requirements.txt
+```
+Run the bot
+```
+python bot.py
+```
+## Configuration
+
+### Donations
+ETH: 0x8143978e687066F635515BD28E0d9D070FAcEb4B
+
+Feel free to use the bot and if you need any help with your bot you can contact me
+
+Twitter: [albaknow](https://twitter.com/albaknow)
--- bot.py
+++ bot.py
@@ -2,77 +2,88 @@ import requests
 import time
 import tweepy
 import secrets_keys as sk
-import pyshorteners as sh
 import os
 import json
 
-s = sh.Shortener()
+# If you are getting many error response code from the request
+# and the bot is failing because of this then please increase this number
+request_try = 10
 
-""" Get this from your Twitter development account (not in repo) 
-you need to create secrets_keys.py file in your cloned repo """
+# Get this from your Twitter development account (not in repo)
+# and you need to add them to secrets_keys.py file in your cloned repo
 consumer_token = sk.consumer_token
 consumer_secret = sk.consumer_secret
 key = sk.key
 secret = sk.secret
+
+# All the etherscan related URLs
 etherscan_base_url = "https://etherscan.io/tx/"
 etherscan_eth_prices = "https://api.etherscan.io/api?module=stats&action=ethprice&apikey="
 etherscan_url_transfers = "https://api.etherscan.io/api?module=account&action=tokennfttx&contractaddress="
 etherscan_url_tx = "https://api.etherscan.io/api?module=account&action=txlistinternal&txhash="
 
-
-"""URL of the latest transfer in etherscan for a defined contract [ERC-721]
-you need to have the contract number (contract_number) and your etherscan Api-Key Token
-(etherscan_key) defined in the secrets_keys.py file"""
+# URL of the latest transfer in etherscan for a defined contract [ERC-721]
+# you need to have the Token contract number (contract_number) and your etherscan Api-Key Token
+# (etherscan_key) and add them to secrets_keys.py file
+# For reference visit https://etherscan.io/apis
 url_transfers = etherscan_url_transfers+sk.contract_number + \
     "&page=1&offset=1&sort=desc&apikey="+sk.etherscan_key
 headers_req = {'Accept': 'application/json'}
 
-"""Seed for the bot to start (latest Txn Hash from Etherscan"""
+# Seed for the bot to start (latest Txn Hash from Etherscan)
+# or you can leave this value empty if you are running the bot for
+# the first time
 past_tx = "0x7e059114725ecacbdce8f8e7824f56c415b02615c027ca1f969d4336aebf48cd"
 latest_tx = ""
 
-"""Run the Bot til the infinite"""
+# Run the Bot til the infinite
 while True:
 
     sum_price = 0
-    """Make a request to get the latest transfer.
-    Try 10 times in case any exception is throw"""
-    for _ in range(10):
+    # Make a request to get the latest transfer.
+    # Try request_try times in case any exception is throw
+    for _ in range(request_try):
         try:
             r_asset = requests.post(url_transfers, headers=headers_req)
             r_json = r_asset.json()
             r_asset.close()
             break
         except (ConnectionResetError, requests.exceptions.RequestException, json.decoder.JSONDecodeError):
-            print(r_asset, "----", r_asset.text)
+            print("Response code---> ", r_asset.status_code,
+                  "\n", "Response text---> ", r_asset.text)
             time.sleep(2)
 
-    """Need to extract the Txn Hash and token of the latest Transfer
-    from etherscan """
+    # Need to extract the Txn Hash and token of the latest Transfer
+    # from etherscan
     Txn_Hash = r_json["result"][0]["hash"]
     latest_tx = Txn_Hash
     token_id = str(r_json["result"][0]["tokenID"])
 
     url_txhash = etherscan_url_tx+str(Txn_Hash)+"&apikey="+sk.etherscan_key
 
-    for _ in range(10):
+    # Make a request to get the Txn Hash details.
+    # Try request_try times in case any exception is throw
+    for _ in range(request_try):
         try:
-            """Make a request to the extracted Txn Hash from the latest Transfer"""
+            # Make a request to the extracted Txn Hash from the latest Transfer
             hash_details = requests.post(url_txhash)
             r_json_details = hash_details.json()
             hash_details.close()
             break
         except (ConnectionResetError, requests.exceptions.RequestException, json.decoder.JSONDecodeError):
-            print(hash_details, "----", hash_details.text)
+            print("Response code---> ", hash_details.status_code,
+                  "\n", "Response text---> ", hash_details.text)
             time.sleep(2)
 
+    # Here we check if the current Transaction/Sale is already posted.
+    # For this we have captured the previous transaction.
     if latest_tx != past_tx:
         past_tx = latest_tx
-        """There can be multiple transactions for one Transfer, so we iterate in the response
-        to get all the transacion and add all the 'values' in each transacion on the Txn Hash"""
+        # There can be multiple transactions for one Transfer, so we iterate in the response
+        # to get all the transactions and add all the 'values' in each transaction on the Txn Hash
         if r_json_details["status"] != "0" and "value" in r_json_details["result"][-1]:
-            """" Do some magic to get all the 'values' and add them to get the real price of
-            the Transfer in ETH """
+            # Some magic to get all the 'values' and add them to get the real price of
+            # the Transfer/Token/NFT in ETH
             for transaction in r_json_details["result"]:
                 asset_price_api = str(transaction["value"])
                 price_size = len(asset_price_api)
@@ -95,58 +106,58 @@ while True:
 
                 sum_price = float(asset_price) + sum_price
 
-            """Final price(cost) of the Transfer in ETH rounded to 6 decimals"""
+            # Final price(cost) of the Transfer in ETH rounded to 6 decimal
             final_price_eth = round(float(sum_price), 6)
 
-            for _ in range(10):
+            # Make a request to get the ETH current prices (ETH/BTC).
+            # Try request_try times in case any exception is throw
+            for _ in range(request_try):
                 try:
-                    """Get the current prices of ETH (BTC and USD)"""
+                    # Get the current prices of ETH (BTC/USD)
                     eth_prices = requests.post(
                         etherscan_eth_prices+sk.etherscan_key)
                     eth_prices_json = eth_prices.json()
                     break
                 except (ConnectionResetError, requests.exceptions.RequestException, json.decoder.JSONDecodeError):
-                    print(eth_prices, "----", eth_prices.text)
+                    print("Response code---> ", eth_prices.status_code,
+                          "\n", "Response text---> ", eth_prices.text)
                     time.sleep(2)
 
-            """Extract the ETH price in USD"""
+            # Extract the ETH price in USD only
             usd_price = float(eth_prices_json["result"]["ethusd"])
+            # Pice in USD of the Token/NFT sale/transfer
             final_price_usd = round(final_price_eth*usd_price, 2)
 
-            """URL of the NFT image"""
-            if len(token_id) == 1:
-                token_id = "000"+token_id
-            elif len(token_id) == 2:
-                token_id = "00"+token_id
-            elif len(token_id) == 3:
-                token_id = "0"+token_id
+            # The following only applies if the URL image of your NFT includes
+            # the token ID if not then you need to adapt the code to your needs
+            # **Contact me if you need help with this
+            # For 10,000 NFT collection from 0 to 9999 (4 digits collection)
+            digits_collection = 4
+            token_id = "0" * (digits_collection - len(token_id)) + token_id
 
-            link_phunk = "https://phunks.s3.us-east-2.amazonaws.com/notpunks/notpunk" + \
+            # URL of the NFT image in you server including the NFT token ID
+            link_nft_image = "https://phunks.s3.us-east-2.amazonaws.com/notpunks/notpunk" + \
                 token_id+".png"
 
-            """Short the URL of the 'Transaction Details' in etherscan"""
+            # URL of the 'Transaction Details' (sale/transfer) in etherscan
             url_tx = etherscan_base_url+Txn_Hash
 
-            """Short the URL of the 'Transaction Details' in etherscan
-            note: this can be fail if the tinyurl server is unreachable"""
-            """ etherscan_tx_url = etherscan_base_url+Txn_Hash
-            url_tx = s.tinyurl.short(etherscan_tx_url) """
-
-            """Download the NFT image to attach it to the twitter post"""
-            response = requests.get(link_phunk)
+            # Download (temporarily) the NFT image to attach it to the twitter post
+            response = requests.get(link_nft_image)
             file = open("nft_image.png", "wb")
             file.write(response.content)
             file.close()
 
-            """Final text to post in twitter with the price in ETH, the price in
-            USD, the image of the NFT, the URL to the Transaction in etherscan
-            and some hash tags"""
-            phunk_sale = "Phunk #"+str(token_id) + " was flipped for Ξ" + \
+            # Final text to post in twitter with a custom message, price in ETH,
+            # the price in USD, the URL to the Transaction in etherscan and some hash tags,
+            # adapt this code to your needs.
+            # **Contact me if you need help with this
+            nft_sale_text = "Phunk #"+str(token_id) + " was flipped for Ξ" + \
                 str(final_price_eth) + " ($"+str(final_price_usd)+")\n"
             hash_tags = "#CryptoPhunks #Phunks #AltPhunks"
-            tweet = phunk_sale + url_tx + "\n" + hash_tags
+            tweet_text = nft_sale_text + url_tx + "\n" + hash_tags
 
-            """Twitter API connection/authentication"""
+            # Twitter API connection/authentication
             auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
             try:
                 redirect_url = auth.get_authorization_url()
@@ -159,16 +170,22 @@ while True:
             api = tweepy.API(auth)
             try:
                 api.verify_credentials()
-                print("Authentication OK")
+                print("Twitter Authentication Success")
             except:
-                print("Authentication error")
+                print("Twitter Authentication Error")
 
-            """Update the status in Twitter"""
-            res_status = api.update_with_media('nft_image.png', status=tweet)
+            # Post the message in your Twitter Bot account
+            # with the image of the sold NFT attached
+            res_status = api.update_with_media(
+                'nft_image.png', status=tweet_text)
+            res_json = res_status.json()
+            if "created_at" in res_json:
+                print("Tweet posted at: ", res_json["created_at"])
 
             """Remove the downloaded image"""
             os.remove("nft_image.png")
 
-    """The Free Api-Key Token from Etherscan only allows Up to 100,000 API calls per day
-    so wait 5 seconds to make a new request to check for a new Transaction"""
+    # The Free Api-Key Token from Etherscan only allows Up to 100,000 API calls per day
+    # so wait some seconds to make a new request to check for a new Transaction
+    # Adapt this wait time as you need
     time.sleep(2)
